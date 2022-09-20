42e8d2ee2779305a7b812c01d85c3bb9e4960752
Henrique Moresco <42219375+morescoh@users.noreply.github.com>
2022-09-20 18:47:21 -0300

    Delete Reading_csv.jl

--- Reading_csv.jl
+++
@@ -1,6 +0,0 @@
-using CSV
-using DataFrames
-include("String_to_Matrix.jl")
-Dados = CSV.read("/home/henriquem/Desktop/PROJECT_01/FILES/gaussian_9.5_3.0_1.5_450_86.csv", DataFrame)
-Matriz = String_to_matrix(Dados[1,2])
-Modelo = eval(Meta.parse(Dados[4,2]))
